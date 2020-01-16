1c952b6ba8d0d76d7f227efd18af4e4cec44f442
dannyphan627 <59778516+dannyphan627@users.noreply.github.com>
2020-01-16 09:17:02 -0800

    Add files via upload
    Interface of Insurance Providers/Companies

---
+++ .NET/Services/IInsuranceProviderService.cs
@@ -0,0 +1,18 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
+using System.Collections.Generic;
+
+namespace Sabio.Services
+{
+    public interface IInsuranceProviderService
+    {
+        int Add(InsuranceProviderAddRequest model, int userId);
+        void Delete(int id);
+        InsuranceProvider Get(int id);
+        Paged<InsuranceProvider> GetAllPagination(int pageIndex, int pageSize);
+        void Update(InsuranceProviderUpdateRequest model, int userId);
+        Paged<InsuranceProvider> SearchInsuranceProviders(string query, int pageSize, int pageIndex);
+        List<InsuranceProvider> GetAllList();
+    }
+}
