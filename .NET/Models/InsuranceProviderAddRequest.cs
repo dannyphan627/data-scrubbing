66bf64352cd87403b2587ed4865a37e4511427ce
dannyphan627 <59778516+dannyphan627@users.noreply.github.com>
2020-01-16 09:15:18 -0800

    Add files via upload
    Models designed to map onto data information when a user attempts to add or update an Insurance Provider

---
+++ .NET/Models/InsuranceProviderAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sabio.Models.Requests
+{
+    public class InsuranceProviderAddRequest
+    { 
+      
+        [Required]
+        [StringLength(200, MinimumLength = 2 )]
+        public string Name { get; set; }
+       
+        [StringLength(255, MinimumLength = 0)]
+        public string SiteUrl { get; set; }
+
+
+    }
+}
---
+++ .NET/Models/InsuranceProviderUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sabio.Models.Requests
+{
+    public class InsuranceProviderUpdateRequest : InsuranceProviderAddRequest, IModelIdentifier
+    {
+       
+       [Range(1, int.MaxValue)]
+        public int Id { get; set; }
+    }
+}
