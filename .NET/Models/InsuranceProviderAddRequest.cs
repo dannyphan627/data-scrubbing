e2b7d340c35aba8aae2fd4cc9c727c5bebae1eb7
dannyphan627 <59778516+dannyphan627@users.noreply.github.com>
2020-01-29 10:28:34 -0800

    Update InsuranceProviderAddRequest.cs

--- .NET/Models/InsuranceProviderAddRequest.cs
+++ .NET/Models/InsuranceProviderAddRequest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
 
-namespace Sabio.Models.Requests
+
+namespace Models.Requests
 {
     public class InsuranceProviderAddRequest
     { 
